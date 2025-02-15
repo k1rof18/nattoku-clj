fdb9a288daefa2e8850785484f30227ab95ba6d8
k-ichirof <abachoppa1892@gmail.com>
2025-02-16 00:45:13 +0900

    word-score

--- ch01/src/ch01/core.clj
+++ ch01/src/ch01/core.clj
@@ -1,7 +1,31 @@
 (ns ch01.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
+(defn- is-not-char? [c] #(not= % c))
+
+(defn- filter-char [text c]
+  (filter (is-not-char? c) (str/split text #"")))
+
+(defn- word-score
+  [text]
+  (count (filter-char text "a")))
+
+;; for / if などの方法に焦点を当てないこと。「a」を省いた文字列に焦点を当てる。
+(defn- word-score-ans
+  [text]
+  (count (str/replace text #"a" "")))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  ""
   [& args]
-  (println "Hello, World!"))
+  (println (word-score "imperative"))
+  (println (word-score "no"))
+  (println (word-score "declarative"))
+  (println (word-score "yes"))
+  (println "")
+  (println (word-score-ans "imperative"))
+  (println (word-score-ans "no"))
+  (println (word-score-ans "declarative"))
+  (println (word-score-ans "yes")))
